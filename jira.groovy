232be825af80dd2803f5e7b037ec3aef931814df
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-05-25 20:35:00 -0300

    Fix Jenkinsfile #5

--- jira.groovy
+++ jira.groovy
@@ -1,9 +1,20 @@
 def createJiraIssue(url, key, issueTypeName, cred, title, link, linkTitle) {
-    def jiraResponse = sh(script: '''curl -D- -u '$cred' -X POST --data '{ \"fields\": { \"project\": { \"key\": \"'$key'\" }, \"summary\": \"'$title'\", \"issuetype\": { \"name\": \"'$issueTypeName'\" } } }' -H 'Content-Type: application/json' '$url'/rest/api/3/issue''', returnStdout: true).trim()
-    def jiraIssueId = (jiraResponse =~ /"id":"([^"]+)"/)[0][1]
-    def jiraKey = (jiraResponse =~ /"key":"([^"]+)"/)[0][1]
-    def jiraSelf = (jiraResponse =~ /"self":"([^"]+)"/)[0][1]
-    sh '''curl -D- -u '$cred' -X POST --data '{ \"object\": { \"url\": \"'$link'\", \"title\": \"'$linkTitle'\" } }' -H 'Content-Type: application/json' '$url'/rest/api/3/issue/'$jiraIssueId'/remotelink'''
+    def jiraResponse = sh(script: """curl -D- -u '$cred' -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
+
+    def jiraIssueIdMatcher = jiraResponse =~ /"id":"([^"]+)"/
+    def jiraKeyMatcher = jiraResponse =~ /"key":"([^"]+)"/
+    def jiraSelfMatcher = jiraResponse =~ /"self":"([^"]+)"/
+
+    def jiraIssueId = jiraIssueIdMatcher ? jiraIssueIdMatcher[0][1] : null
+    def jiraKey = jiraKeyMatcher ? jiraKeyMatcher[0][1] : null
+    def jiraSelf = jiraSelfMatcher ? jiraSelfMatcher[0][1] : null
+
+    if (jiraIssueId && jiraKey && jiraSelf) {
+        sh(script: """curl -D- -u '$cred' -X POST --data '{ "object": { "url": "${link}", "title": "${linkTitle}" } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue/${jiraIssueId}/remotelink""")
+    } else {
+        error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
+    }
+
     return [jiraIssueId: jiraIssueId, jiraKey: jiraKey, jiraSelf: jiraSelf]
 }
 
