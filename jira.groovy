a3544c57c8cb601616624dbbd34a36bfc261913d
Gaston Francois <gfrancois@itba.edu.ar>
2024-05-25 22:44:06 -0300

    test new regex #2

--- jira.groovy
+++ jira.groovy
@@ -21,23 +21,9 @@ def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
 def createJiraIssueWithComment(url, key, issueTypeName, creds, title, link, linkTitle, comment) {
     def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
 
-    def jiraIssueIdMatcher = (jiraResponse =~ /"id":"([^\s"]+)"/)
-    def jiraKeyMatcher = (jiraResponse =~ /"key":"([^\s"]+)"/)
-    def jiraSelfMatcher = (jiraResponse =~ /"self":"([^\s"]+)"/)
-
-    def jiraIssueId = null
-    def jiraKey = null
-    def jiraSelf = null
-
-    if (jiraIssueIdMatcher.find()) {
-        jiraIssueId = jiraIssueIdMatcher.group(1)
-    }
-    if (jiraKeyMatcher.find()) {
-        jiraKey = jiraKeyMatcher.group(1)
-    }
-    if (jiraSelfMatcher.find()) {
-        jiraSelf = jiraSelfMatcher.group(1)
-    }
+    def jiraIssueId = (jiraResponse =~ /"id":"([^\s"]+)"/)[0][1]
+    def jiraKey = (jiraResponse =~ /"key":"([^\s"]+)"/)[0][1]
+    def jiraSelf = (jiraResponse =~ /"self":"([^\s"]+)"/)[0][1]
 
     if (!jiraIssueId || !jiraKey || !jiraSelf) {
         error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
