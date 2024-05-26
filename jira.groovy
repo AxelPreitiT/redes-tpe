643e4dbb5f3a425ebfbb7f4ae0d97ef718120489
Gaston Francois <gfrancois@itba.edu.ar>
2024-05-26 15:58:36 -0300

    new sintax in curl

--- jira.groovy
+++ jira.groovy
@@ -1,5 +1,5 @@
 def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
-    def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
+    def jiraResponse = sh(script: 'curl -D- -u ' + creds + ' -X POST --data \'{ "fields": { "project": { "key": "' + key + '", "summary": "' + title + '", "issuetype": { "name": "' + issueTypeName + '" } } }\' -H \'Content-Type: application/json\' ' + url + '/rest/api/3/issue', returnStdout: true).trim()
 
     def jiraIssueId = (jiraResponse =~ /"id":"([^\s"]+)"/)[0][1]
     def jiraKey = (jiraResponse =~ /"key":"([^\s"]+)"/)[0][1]
@@ -9,7 +9,7 @@ def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
         error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
     }
     
-    sh(script: """curl -D- -u ${creds} -X POST --data '{ "object": { "url": "${link}", "title": "${linkTitle}" } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue/${jiraIssueId}/remotelink""")
+    sh(script: 'curl -D- -u ' + creds + ' -X POST --data \'{ "object": { "url": "' + link + '", "title": "' + linkTitle + '" } }\' -H \'Content-Type: application/json\' ' + url + '/rest/api/3/issue/' + jiraIssueId + '/remotelink')
     
     return [jiraIssueId: jiraIssueId, jiraKey: jiraKey, jiraSelf: jiraSelf]
 }
