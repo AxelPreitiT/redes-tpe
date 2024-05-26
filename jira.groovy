debc13f05271460c87877ae3beca036a00b33bb7
Gaston Francois <gfrancois@itba.edu.ar>
2024-05-25 22:05:34 -0300

    try to fix with unique fuction

--- jira.groovy
+++ jira.groovy
@@ -18,7 +18,21 @@ def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
     return [jiraIssueId: jiraIssueId, jiraKey: jiraKey, jiraSelf: jiraSelf]
 }
 
-def addJiraComment(comment, jiraKey) {
+def createJiraIssueWithComment(url, key, issueTypeName, creds, title, link, linkTitle, comment) {
+    def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
+
+    def jiraIssueIdMatcher = (jiraResponse =~ /"id":"([^"]+)"/)
+    def jiraKeyMatcher = (jiraResponse =~ /"key":"([^"]+)"/)
+    def jiraSelfMatcher = (jiraResponse =~ /"self":"([^"]+)"/)
+
+    def jiraIssueId = jiraIssueIdMatcher ? jiraIssueIdMatcher[0][1] : null
+    def jiraKey = jiraKeyMatcher ? jiraKeyMatcher[0][1] : null
+    def jiraSelf = jiraSelfMatcher ? jiraSelfMatcher[0][1] : null
+
+    if (!jiraIssueId || !jiraKey || !jiraSelf) {
+        error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
+    }
+
     jiraComment body: comment, issueKey: jiraKey
 }
 
