013a3b5819bed5a83ab807a18c479ddc7cc67dcf
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-05-25 21:14:33 -0300

    Fix Jenkinsfile #8

--- jira.groovy
+++ jira.groovy
@@ -1,26 +1,16 @@
-def createJiraIssue(url, key, issueTypeName, credId, title, link, linkTitle) {
-    def jiraIssueId
-    def jiraKey
-    def jiraSelf
+def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
+    def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
 
-    withCredentials([usernamePassword(credentialsId: credId, usernameVariable: 'JIRA_USER', passwordVariable: 'JIRA_TOKEN')]) {
-        def jiraResponse = sh(script: """curl -D- -u '$JIRA_USER:$JIRA_TOKEN' -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
+    def jiraIssueId = jiraIssueIdMatcher ? jiraIssueIdMatcher[0][1] : null
+    def jiraKey = jiraKeyMatcher ? jiraKeyMatcher[0][1] : null
+    def jiraSelf = jiraSelfMatcher ? jiraSelfMatcher[0][1] : null
 
-        def jiraIssueIdMatcher = jiraResponse =~ /"id":"([^"]+)"/
-        def jiraKeyMatcher = jiraResponse =~ /"key":"([^"]+)"/
-        def jiraSelfMatcher = jiraResponse =~ /"self":"([^"]+)"/
-
-        jiraIssueId = jiraIssueIdMatcher ? jiraIssueIdMatcher[0][1] : null
-        jiraKey = jiraKeyMatcher ? jiraKeyMatcher[0][1] : null
-        jiraSelf = jiraSelfMatcher ? jiraSelfMatcher[0][1] : null
-
-        if (!jiraIssueId || !jiraKey || !jiraSelf) {
-            error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
-        }
-
-        sh(script: """curl -D- -u '$JIRA_USER:$JIRA_TOKEN' -X POST --data '{ "object": { "url": "${link}", "title": "${linkTitle}" } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue/${jiraIssueId}/remotelink""")
+    if (!jiraIssueId || !jiraKey || !jiraSelf) {
+        error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
     }
-
+    
+    sh(script: """curl -D- -u ${creds} -X POST --data '{ "object": { "url": "${link}", "title": "${linkTitle}" } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue/${jiraIssueId}/remotelink""")
+    
     return [jiraIssueId: jiraIssueId, jiraKey: jiraKey, jiraSelf: jiraSelf]
 }
 
