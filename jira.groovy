cd8b74056dbae31da4feed1d7c241a42e5624073
Gaston Francois <gfrancois@itba.edu.ar>
2024-05-25 22:50:59 -0300

    add new fuction

--- jira.groovy
+++ jira.groovy
@@ -1,13 +1,9 @@
 def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
     def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
 
-    def jiraIssueIdMatcher = (jiraResponse =~ /"id":"([^"]+)"/)
-    def jiraKeyMatcher = (jiraResponse =~ /"key":"([^"]+)"/)
-    def jiraSelfMatcher = (jiraResponse =~ /"self":"([^"]+)"/)
-
-    def jiraIssueId = jiraIssueIdMatcher ? jiraIssueIdMatcher[0][1] : null
-    def jiraKey = jiraKeyMatcher ? jiraKeyMatcher[0][1] : null
-    def jiraSelf = jiraSelfMatcher ? jiraSelfMatcher[0][1] : null
+    def jiraIssueId = (jiraResponse =~ /"id":"([^\s"]+)"/)[0][1]
+    def jiraKey = (jiraResponse =~ /"key":"([^\s"]+)"/)[0][1]
+    def jiraSelf = (jiraResponse =~ /"self":"([^\s"]+)"/)[0][1]
 
     if (!jiraIssueId || !jiraKey || !jiraSelf) {
         error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
@@ -18,24 +14,9 @@ def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
     return [jiraIssueId: jiraIssueId, jiraKey: jiraKey, jiraSelf: jiraSelf]
 }
 
-def createJiraIssueWithComment(url, key, issueTypeName, creds, title, link, linkTitle, comment) {
-    def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
-
-    def jiraIssueId = (jiraResponse =~ /"id":"([^\s"]+)"/)[0][1]
-    def jiraKey = (jiraResponse =~ /"key":"([^\s"]+)"/)[0][1]
-    def jiraSelf = (jiraResponse =~ /"self":"([^\s"]+)"/)[0][1]
-
-    if (!jiraIssueId || !jiraKey || !jiraSelf) {
-        error "Failed to extract necessary fields from Jira response: ${jiraResponse}"
-    }
-
+def commentJiraIssue(comment, jiraKey) {
     jiraComment body: comment, issueKey: jiraKey
 }
-
-def createJiraIssueAlone(url, key, issueTypeName, creds, title, link, linkTitle, comment) {
-    def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
-}
-
 
 
 return this
