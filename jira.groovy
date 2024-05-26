1f4c2e4a3f2bf6fd693bf99b5b33fe7551cbcbfe
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-05-25 21:33:38 -0300

    Fix Jenkinsfile #9

--- jira.groovy
+++ jira.groovy
@@ -1,6 +1,10 @@
 def createJiraIssue(url, key, issueTypeName, creds, title, link, linkTitle) {
     def jiraResponse = sh(script: """curl -D- -u ${creds} -X POST --data '{ "fields": { "project": { "key": "${key}" }, "summary": "${title}", "issuetype": { "name": "${issueTypeName}" } } }' -H 'Content-Type: application/json' ${url}/rest/api/3/issue""", returnStdout: true).trim()
 
+    def jiraIssueIdMatcher = (jiraResponse =~ /"id":"([^"]+)"/)
+    def jiraKeyMatcher = (jiraResponse =~ /"key":"([^"]+)"/)
+    def jiraSelfMatcher = (jiraResponse =~ /"self":"([^"]+)"/)
+
     def jiraIssueId = jiraIssueIdMatcher ? jiraIssueIdMatcher[0][1] : null
     def jiraKey = jiraKeyMatcher ? jiraKeyMatcher[0][1] : null
     def jiraSelf = jiraSelfMatcher ? jiraSelfMatcher[0][1] : null
